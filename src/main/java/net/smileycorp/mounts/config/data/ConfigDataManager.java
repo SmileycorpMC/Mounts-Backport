4562faf0bab0e1df85da411c38916ad7c5b0be3d
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-09-11 23:54:47 +0100

    add config data version check

--- src/main/java/net/smileycorp/mounts/config/data/ConfigDataManager.java
+++ src/main/java/net/smileycorp/mounts/config/data/ConfigDataManager.java
@@ -1,9 +1,14 @@
 package net.smileycorp.mounts.config.data;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.smileycorp.mounts.common.MountsLogger;
 import org.apache.commons.io.FileUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.util.Collections;
 
@@ -12,19 +17,20 @@ public class ConfigDataManager {
     public static Path CONFIG_FOLDER;
 
     public static void init() {
+        MountsLogger.clearLog(true);
         MountsLogger.blankLine();
         MountsLogger.heading("CHECKING CONFIG DATA");
         MountsLogger.blankLine();
         CONFIG_FOLDER = Paths.get(new File("config/mounts").getAbsolutePath());
-        if (shouldGenerateFiles(CONFIG_FOLDER.getParent())) generateData();
+        if (shouldGenerateFiles()) generateData();
         else MountsLogger.logInfo("Config data files are up to date, skipping data/asset generation");
     }
 
-    public static boolean shouldGenerateFiles(Path folder) {
+    public static boolean shouldGenerateFiles() {
         if (isUpToDate()) return false;
         try {
             if (CONFIG_FOLDER.toFile().exists()) {
-                File backup = folder.resolve("mounts-backup").toFile();
+                File backup = CONFIG_FOLDER.getParent().resolve("mounts-backup").toFile();
                 backup.mkdir();
                 FileUtils.deleteDirectory(backup);
                 FileUtils.copyDirectory(CONFIG_FOLDER.toFile(), backup);
@@ -48,7 +54,27 @@ public class ConfigDataManager {
     }
 
     private static boolean isUpToDate() {
-        return CONFIG_FOLDER.toFile().exists();
+        File config_file = CONFIG_FOLDER.resolve("mounts-info.json").toFile();
+        if (!config_file.isFile()) {
+            MountsLogger.logInfo("Hordes data does not exist, generating new files.");
+            return false;
+        }
+        try (FileSystem mod = FileSystems.newFileSystem(ConfigDataManager.class.getProtectionDomain().getCodeSource().getLocation().toURI(),
+                Collections.emptyMap())) {
+            JsonParser parser = new JsonParser();
+            JsonObject configJson = parser.parse(new FileReader(config_file)).getAsJsonObject();
+            if (configJson.get("data_version").getAsInt() < 0) return true;
+            JsonObject modJson = parser.parse(new BufferedReader(
+                    new InputStreamReader(Files.newInputStream(mod.getPath("config_defaults/mounts-info.json"))))).getAsJsonObject();
+            if (configJson.get("data_version").getAsInt() >= modJson.get("data_version").getAsInt() && (!modJson.has("dev_version")
+                    |! configJson.has("dev_version") || configJson.get("dev_version").getAsInt() >= modJson.get("dev_version").getAsInt()))
+                return true;
+            MountsLogger.logInfo("Mounts data is not up to date or set to pack author mode, generating new files.");;
+            return false;
+        } catch (Exception e) {
+            MountsLogger.logError("Failed data version check", e);
+            return true;
+        }
     }
 
     private static void copyFileFromMod(Path path) {
