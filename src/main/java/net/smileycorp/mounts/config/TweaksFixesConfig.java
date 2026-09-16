28056834ed51d9bc5ebc6dcf1cbf8779f1c40671
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-09-16 17:01:07 +0100

    add config setting for horse dismounting

--- src/main/java/net/smileycorp/mounts/config/TweaksFixesConfig.java
+++ src/main/java/net/smileycorp/mounts/config/TweaksFixesConfig.java
@@ -17,6 +17,8 @@ public class TweaksFixesConfig {
     public static boolean ridersControlAnimals;
     public static boolean passengersCantHitEachother;
     public static boolean controlledMountsOwnPathing;
+    public static boolean horsesDontDismountMobs;
+
     public static void syncConfig(FMLPreInitializationEvent event) {
         Configuration config = new Configuration(new File(event.getModConfigurationDirectory().getPath() + "/mounts/tweaks_fixes.cfg"));
         try{
@@ -28,6 +30,7 @@ public class TweaksFixesConfig {
             ridersControlAnimals = config.getBoolean("ridersControlAnimals", "fixes", true, "Are animals movements controlled by their rider? (Used to make jockey mobs work)");
             passengersCantHitEachother = config.getBoolean("passengersCantHitEachother", "fixes", true, "Are projectiles shot by passengers prevented from hitting other passengers on the same entity? (Required to prevent camel husk jockey infighting)");
             controlledMountsOwnPathing = config.getBoolean("controlledMountsOwnPathing", "fixes", true, "Do controlled mounted entities use their own pathing instead of the controlling passenger's pathing? (Fixes issues with large hitbox mounts getting stuck on the edges of blocks, also required to prevent camel husk jockey ai bugging)");
+            horsesDontDismountMobs = config.getBoolean("horsesDontDismountMobs", "fixes", true, "Are horses prevented from dismounting mobs that are riding them? (Fixes baby zombie jockeys not being able to ride horses)");
         } catch(Exception e) {
         } finally {
             if (config.hasChanged()) config.save();
