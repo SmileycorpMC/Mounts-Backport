553d71853e6d363c2c98de836e3b87b85711329b
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-09-21 00:01:50 +0100

    Update MixinWorldEntitySpawner.java

--- src/main/java/net/smileycorp/mounts/mixin/MixinWorldEntitySpawner.java
+++ src/main/java/net/smileycorp/mounts/mixin/MixinWorldEntitySpawner.java
@@ -16,8 +16,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.UUID;
-
 @Mixin(WorldEntitySpawner.class)
 public class MixinWorldEntitySpawner {
 
