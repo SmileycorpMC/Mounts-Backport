617319a3d894405848632ab09d7b91ca9700b3c5
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-09-20 22:01:22 +0100

    Update MixinWorldEntitySpawner.java

--- src/main/java/net/smileycorp/mounts/mixin/MixinWorldEntitySpawner.java
+++ src/main/java/net/smileycorp/mounts/mixin/MixinWorldEntitySpawner.java
@@ -2,8 +2,6 @@ package net.smileycorp.mounts.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.world.World;
